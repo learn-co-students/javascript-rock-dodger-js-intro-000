f195d081bd5d41342e9ea09c00e6a7b4213dfdcb
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2016-10-25 10:19:46 -0400

    remove rocks from ROCK in endGame()

--- index.js
+++ index.js
@@ -45,6 +45,8 @@ function createRock(x) {
 
   ROCKS.push(rock)
 
+  return rock
+
 }
 
    function checkCollision(rock) {
@@ -68,28 +70,22 @@ function createRock(x) {
 function endGame() {
  // document.location.reload(true);
  clearInterval(null);
- console.log("ended")
+ console.log("ended");
+
+ ROCKS.forEach(function(rock){rock.remove()})
+
+
 }
 
 function moveDodger(e) {
+  // connect functions to keys
    if (e.which === LEFT_ARROW){moveDodgerLeft(); }
    if (e.which === RIGHT_ARROW){moveDodgerRight();}
-  // implement me!
-  /**
-   * This function should call `moveDodgerLeft()`
-   * if the left arrow is pressed and `moveDodgerRight()`
-   * if the right arrow is pressed. (Check the constants
-   * we've declared for you above.)
-   * And be sure to use the functions declared below!
-   */
+
 }
 
 function moveDodgerLeft() {
-  // implement me!
-  /**
-   * This function should move DODGER to the left
-   * (mabye 4 pixels?). Use window.requestAnimationFrame()!
-   */
+  // move dodger to left 10px
 
    var leftNumbers = DODGER.style.left.replace('px','')
     var left = parseInt(leftNumbers,10)
@@ -103,11 +99,7 @@ function moveDodgerLeft() {
 
 
 function moveDodgerRight() {
-  // implement me!
-  /**
-   * This function should move DODGER to the left
-   * (mabye 4 pixels?). Use window.requestAnimationFrame()!
-   */
+  //move dodger to right 10px
   var leftNumbers = dodger.style.left.replace('px','')
     var left = parseInt(leftNumbers,10)
     if(left < 360){
