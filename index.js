ae0e5cf578997c8828f3bdb9959bd0c4b77d17fe
punctual-sailor-2214 <bchett286@yahoo.com>
2019-04-29 15:49:00 +0000

    Done.

--- index.js
+++ index.js
@@ -117,12 +117,8 @@ function createRock(x) {
  * Finally, alert "YOU LOSE!" to the player.
  */
 function endGame() {
-  while (ROCKS.length > 0) {
-    GAME.removeChild(ROCKS[0]);
-    ROCKS.shift();
-  }
   clearInterval(gameInterval);
-
+  ROCKS.forEach(function(rock) { rock.remove() })
   window.removeEventListener('keydown', moveDodger);
   START.innerHTML = 'Play again?';
   START.style.display = 'inline';
