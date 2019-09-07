1aa5d0d0e0f037de3112daba569a287c3459d2ed
Hamza Qureshi <hamza.qureshi97@gmail.com>
2019-09-07 19:28:36 +0000

    Done.

--- index.js
+++ index.js
@@ -28,14 +28,14 @@ function checkCollision(rock) {
   if (top > 360) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
 
-    // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
+    /** : The DODGER is 40 pixels wide -- how do we get the right edge?*/
     const dodgerRightEdge = dodgerLeftEdge + 40;
 
 
 
     const rockLeftEdge = positionToInteger(rock.style.left)
 
-    // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
+    // : The rock is 20 pixel's wide -- how do we get the right edge?
     const rockRightEdge = rockLeftEdge + 20;
 
 
